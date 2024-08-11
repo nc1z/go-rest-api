adb093f26c06005cb17741ed4627f3335384da7e
nc1z <ncxwork@gmail.com>
2024-08-11 11:23:02 +0800

    feat: add tests

---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHomeHandler(t *testing.T) {
+	mockResponse := `{"message":"Task Management API with Golang and Gin"}`
+	r := SetUpRouter()
+	r.GET("/", HomeHandler)
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	responseData, _ := io.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
+}
